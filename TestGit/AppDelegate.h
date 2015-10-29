508374a921fe67d02ea868f907c03b67618ae2bd
@gaobin <534628410@qq.com>
2015-10-29 09:39:11 +0800

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by gaobin on 15/10/29.
+//  Copyright © 2015年 gaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
